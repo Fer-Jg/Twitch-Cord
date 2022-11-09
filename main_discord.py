24ac22bc83bcdb29f302fe1e9405465ec9917cb2
Fer-Jgu <fernagur@gmail.com>
2022-11-09 12:49:01 -0600

    Moved everything discord-related inside discord's class

--- main_discord.py
+++ main_discord.py
@@ -39,42 +39,24 @@ class Bot(commands.Bot):
     def set_creator(self):
         self.creator = self.get_user(self.creator_id)
 
-def intents():
-    set = discord.Intents.default()
-    set.members = True
-    set.message_content = True
-    return set
+    async def on_disconnect(self) -> None:
+        print("bot disconnected")
 
-bot = Bot(
-    command_prefix="=", 
-    owner_id=347524158181212161, 
-    intents = intents(), 
-    case_insensitive=True,
-    status=discord.Status.online
-    )
+    async def on_connect(self) -> None:
+        print("bot connected")
 
-@bot.event
-async def on_ready() -> None:
-    bot.set_creator()
-    bot.bot_ready = True
-    activity = discord.Streaming(name=bot.streamer.name,
-                                platform=bot.streamer.platform,
-                                details=f"con {bot.streamer.name}",
-                                url=bot.streamer.link)
-    await bot.change_presence(activity=activity)
-    print("bot is ready")
+    async def on_ready(self) -> None:
+        self.set_creator()
+        self.bot_ready = True
+        activity = discord.Streaming(name=self.streamer.name,
+                                    platform=self.streamer.platform,
+                                    details=f"con {self.streamer.name}",
+                                    url=self.streamer.link)
+        await self.change_presence(activity=activity)
+        print("bot is ready")
 
-@bot.event
-async def on_disconnect() -> None:
-    print("bot disconnected")
-
-@bot.event
-async def on_connect() -> None:
-    print("bot connected")
-
-@bot.before_invoke
-async def on_command_before_invoke(ctx : commands.Context) -> None:
-    if not ctx.bot.bot_ready: raise BotNotReady
+    async def on_command_before_invoke(self, ctx : commands.Context) -> None:
+        if not ctx.bot.bot_ready: raise BotNotReady
 
 bot = DiscordBot(
     command_prefix="=", 
